aa9937f2ef4c0970cd0de911ca85f023ecc51af4
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-01-21 00:44:43 -0600

    finished department seeds

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -2,10 +2,21 @@ use employees;
 
 INSERT INTO department (name)
 VALUES
-   ("")
-   ("")
-   ("")
-   ("")
+   ("Sales"),
+   ("Finance"),
+   ("Engineering"),
+   ("Legal");
+
+INSERT INTO role (title,salary,department_id)
+VALUES
+   (""),
+   (""),
+   (""),
+   (""),
+   (""),
+   (""),
+   (""),
+   ("");
   
    
    
