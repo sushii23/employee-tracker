3490b8faa1700bbfb6fca5913ef6993cfa26a4e1
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-01-23 00:14:45 -0600

    finisded role values

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -5,18 +5,30 @@ VALUES
    ("Sales"),
    ("Finance"),
    ("Engineering"),
-   ("Legal");
+   ("Human Resources");
 
 INSERT INTO role (title,salary,department_id)
 VALUES
-   (""),
-   (""),
-   (""),
-   (""),
-   (""),
-   (""),
-   (""),
-   ("");
+   ("Sales Team Member", 30000,0),
+   ("Sales Lead Expert",70000,0),
+   ("Finance Lead",80000,1),
+   ("Finace Manager",100000,1),
+   ("Software Engineer",85000,2),
+   ("Engineer Lead",150000,2),
+   ("HR Member",60000,3),
+   ("HR Manager",80000,3);
+
+
+INSERT INTO employee (last_name, first_name, role_id, manager_id)
+VALUES
+   ("")
+   ("")
+   ("")
+   ("")
+   ("")
+   ("")
+   ("")
+   ("")
   
    
    
