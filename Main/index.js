9bd550514649f709d4f801833e38001e2ad1e3cb
Adrian Deon <sushdawg@Adrians-MBP.lan1>
2023-01-23 15:33:04 -0600

    added functions

--- Main/index.js
+++ Main/index.js
@@ -50,11 +50,64 @@ function showMainPrompts() {
   } 
 
   function showEmployees() {
-    db.findAllEmployees()
+    db.searchAllEmployees()
       .then(([rows]) => {
         let employees = rows;
         console.log("\n");
         console.table(employees);
+      })
+      .then(() => showMainPrompts());
+  }
+  
+  function updateEmployeeRole() {
+    db.searchAllEmployees()
+      .then(([rows]) => {
+        let employees = rows;
+        const employeeChoices = employees.map(({ id, last_name, first_name }) => ({
+          name: `${last_name} ${first_name}`,
+          value: id
+        }));
+  
+        prompt([
+          {
+            type: "list",
+            name: "employeeId",
+            message: "Chose an employee's role to update.",
+            choices: employeeChoices
+          }
+        ])
+          .then(res => {
+            let employeeId = res.employeeId;
+            db.searchAllRoles()
+              .then(([rows]) => {
+                let roles = rows;
+                const roleChoices = roles.map(({ id, title }) => ({
+                  name: title,
+                  value: id
+                }));
+  
+                prompt([
+                  {
+                    type: "list",
+                    name: "roleId",
+                    message: "What role would you like to assign the selected employee?",
+                    choices: roleChoices
+                  }
+                ])
+                  .then(res => db.updateEmployeeRole(employeeId, res.roleId))
+                  .then(() => console.log("Updated employee's role"))
+                  .then(() => showMainPrompts())
+              });
+          });
+      })
+  }
+
+  function showRoles() {
+    db.searchAllRoles()
+      .then(([rows]) => {
+        let roles = rows;
+        console.log("\n");
+        console.table(roles);
       })
       .then(() => showMainPrompts());
   }
