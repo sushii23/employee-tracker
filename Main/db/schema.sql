b2b7eb43f0cbc1ca567683158d59d0983ff977a6
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-01-25 01:41:37 -0600

    fixed seeds

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,35 +1,59 @@
-IF EXISTS (SELECT name FROM sys.databases WHERE name = 'employees')
-BEGIN
-    DROP DATABASE employees;
-END
+-- IF EXISTS (SELECT name FROM sys.databases WHERE name = "employees")
+-- BEGIN
+--     DROP DATABASE employees;
+-- END
 
+-- CREATE DATABASE employees;
+-- GO
+-- USE employees;
+-- GO
+DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
-GO
+
 USE employees;
-GO
 
 CREATE TABLE department (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) 
 );
 
+-- CREATE TABLE role (
+--     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+--     title VARCHAR(30) ,
+--     salary DECIMAL UNSIGNED ,
+--     department_id INT UNSIGNED ,
+--     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
+--     INDEX (department_id)
+-- );
+
+-- CREATE TABLE employee (
+--     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+--     last_name VARCHAR(30) ,
+--     first_name VARCHAR(30) ,
+--     role_id INT UNSIGNED ,
+--     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+--     manager_id INT UNSIGNED,
+--     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+--     INDEX (role_id),
+--     INDEX (manager_id)
+-- );
 CREATE TABLE role (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) ,
-    salary DECIMAL UNSIGNED ,
-    department_id INT UNSIGNED ,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
-    INDEX (department_id)
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    last_name VARCHAR(30) ,
-    first_name VARCHAR(30) ,
-    role_id INT UNSIGNED ,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    manager_id INT UNSIGNED,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
-    INDEX (role_id),
-    INDEX (manager_id)
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  last_name VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -9,26 +9,26 @@ VALUES
 
 INSERT INTO role (title,salary,department_id)
 VALUES
-   ("Sales Team Member", 30000,0),
-   ("Sales Lead Expert",70000,0),
-   ("Finance Lead",80000,1),
-   ("Finace Manager",100000,1),
-   ("Software Engineer",85000,2),
-   ("Engineer Lead",150000,2),
-   ("HR Member",60000,3),
-   ("HR Manager",80000,3);
+   ("Sales Team Member", 30000, 0),
+   ("Sales Lead Expert", 70000, 0),
+   ("Finance Lead", 80000, 1),
+   ("Finace Manager", 100000, 1),
+   ("Software Engineer", 85000, 2),
+   ("Engineer Lead", 150000, 2),
+   ("HR Member", 60000, 3),
+   ("HR Manager", 80000, 3);
 
 
 INSERT INTO employee (last_name, first_name, role_id, manager_id)
 VALUES
-   ("Mason","Brick",0,NULL)
-   ("Madden","Steve",1,8)
-   ("Hill","Tommy",2,2)
-   ("Carter","Jimmy",3,NULL)
-   ("Johnson","Bobby",4,NULL)
-   ("Smith","Eddie",5,NULL)
-   ("Blackson","Micheal",6,4)
-   ("Jenkins","Leroy",7,6)
+   ("Mason", "Brick", 0, NULL),
+   ("Madden", "Steve", 1, 8),
+   ("Hill", "Tommy", 2, 2),
+   ("Carter", "Jimmy", 3, NULL),
+   ("Johnson", "Bobby", 4, NULL),
+   ("Smith", "Eddie", 5, NULL),
+   ("Blackson", "Micheal", 6, 4),
+   ("Jenkins", "Leroy", 7, 6);
   
    
    
